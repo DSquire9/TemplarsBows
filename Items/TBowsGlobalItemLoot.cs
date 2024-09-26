731655a7e9e2b098e7e823da3a03fc82508a2caf
DSquire9 <dss9593@rit.edu>
2024-09-25 20:48:34 -0400

    Added Yumi to golden lockbox's loot pool

---
+++ Items/TBowsGlobalItemLoot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TemplarsBows.Items
+{
+    public class TBowsGlobalItemLoot : GlobalItem
+    {
+        public override bool InstancePerEntity => false;
+
+        public override void ModifyItemLoot(Item item, ItemLoot loot)
+        {
+            switch (item.type)
+            {
+                case ItemID.LockBox:
+                    loot.Add(new CommonDrop(ModContent.ItemType<Yumi>(), 14));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
