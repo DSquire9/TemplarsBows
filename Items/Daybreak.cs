c4264e5a56c551fb45d874b2915252a070511dd6
DSquire9 <dss9593@rit.edu>
2024-09-27 11:29:54 -0400

    Added temp values to Daybreak and Thorns

--- Items/Daybreak.cs
+++ Items/Daybreak.cs
@@ -14,7 +14,23 @@ namespace TemplarsBows.Items
     {
         public override void SetDefaults()
         {
-            
+            Item.damage = 26;
+            Item.DamageType = DamageClass.Ranged;
+            Item.useAmmo = AmmoID.Arrow;
+            Item.shootSpeed = 7f;
+            Item.noMelee = true;
+            Item.width = 40;
+            Item.height = 40;
+            Item.scale = .85f;
+            Item.useTime = 22;
+            Item.useAnimation = 22;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.knockBack = 1;
+            Item.crit = 4;
+            Item.value = 10000;
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item5;
+            Item.autoReuse = false;
         }
 
         public override void AddRecipes()
